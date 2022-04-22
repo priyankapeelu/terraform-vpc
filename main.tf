51c596d302b1f7b73bad9ee659b35c3e8daacc72
priyankapeelu <priyankapeelu@icloud.com>
2022-04-22 18:16:47 +0530

    terraform variablescd tt

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "vpc" {
-  source   = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
-  ENV = var.ENV
+  source           = "./vendor/modules/vpc"
+  VPC_CIDR         = var.VPC_CIDR
+  ENV              = var.ENV
+  SUBNET_CIDR      = var.SUBNET_CIDR
+  AZ               = var.AZ
 }
