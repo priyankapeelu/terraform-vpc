b4b02118885154154cdd56b223592e920fb52d23
priyankapeelu <priyankapeelu@icloud.com>
2022-04-26 17:51:06 +0530

    terraform variablescd tt

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   VPC_CIDR            = var.VPC_CIDR
   ENV                 = var.ENV
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  PUBLIC_SUBNET_CIDR  = PUBLIC_SUBNET_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   AZ                  = var.AZ
   DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
