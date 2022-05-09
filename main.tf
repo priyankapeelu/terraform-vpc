a0954199674556df61f60f70239cff254ff8978e
priyankapeelu <priyankapeelu@icloud.com>
2022-05-09 15:03:55 +0530

    terraform variablescd tt

--- main.tf
+++ main.tf
@@ -1,20 +1,15 @@
 module "vpc" {
-  source              = "./vendor/modules/vpc"
-  VPC_CIDR            = var.VPC_CIDR
-  ENV                 = var.ENV
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  AZ                  = var.AZ
-  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
-}
-
-module "redis" {
-  source                     = "./vendor/modules/redis"
-  ENV                        = var.ENV
-  ELASTICACHE_NODE_TYPE      = var.ELASTICACHE_NODE_TYPE
-  ELASTICACHE_NODE_COUNT     = var.ELASTICACHE_NODE_COUNT
-  ELASTICACHE_PORT           = var.ELASTICACHE_PORT
-  ELASTICACHE_ENGINE_VERSION = var.ELASTICACHE_ENGINE_VERSION
+  source                  = "./vendor/modules/vpc"
+  VPC_CIDR                = var.VPC_CIDR
+  ENV                     = var.ENV
+  PRIVATE_SUBNET_CIDR     = var.PRIVATE_SUBNET_CIDR
+  PUBLIC_SUBNET_CIDR      = var.PUBLIC_SUBNET_CIDR
+  AZ                      = var.AZ
+  DEFAULT_VPC_ID          = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR        = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT          = var.DEFAULT_VPC_RT
+  HOSTEDZONE_PRIVATE_ID   = var.HOSTEDZONE_PRIVATE_ID
+  HOSTEDZONE_PRIVATE_ZONE = var.HOSTEDZONE_PRIVATE_ZONE
+  HOSTEDZONE_PUBLIC_ID    = var.HOSTEDZONE_PUBLIC_ID
+  HOSTEDZONE_PUBLIC_ZONE  = var.HOSTEDZONE_PUBLIC_ZONE
 }
